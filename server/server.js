e8a9b1f9c864468effcedace0cc57ab26939bbfa
Hasnain Raza <hasnainraza1072@gmail.com>
2023-03-27 20:50:25 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const app = require('express')();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
